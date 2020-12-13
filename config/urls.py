e29402615ca8ef4487fe5b418c4f834ad46ae7ee
yujin-hello <yjyj4281@daum.net>
2020-12-13 13:24:39 +0900

    covid_19

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+# config\urls.py
+from django.contrib import admin
+from django.urls import path, include
+from chart import views as chart_views
+
+urlpatterns = [
+    path('', chart_views.home, name='home'),
+    path('chart/', include('chart.urls')),
+    path('admin/', admin.site.urls),
+]
